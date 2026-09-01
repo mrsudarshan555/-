2c2831c839c123396b9d300ed14231028cb97590
mrsudarshan555 <skboy809226@gmail.com>
2026-09-01 06:52:59 +0530

    refactor: remove project files
    Clean up the repository by removing obsolete build configurations, workflows, and documentation.

--- src/services/offline/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './offlineAiTypes';
-export * from './nativeLlmBridge';
-export * from './localModelManager';
-export * from './offlineMayraProvider';
-export * from './hybridAiRouter';
